6f503bd91b5ea94874ddafca1f9cceae885c50c3
Wact. B. Prot <wactbprot@gmail.com>
2022-07-06 21:29:35 +0200

    more info for proc agent

--- src/dxrt/model.clj
+++ src/dxrt/model.clj
@@ -8,8 +8,12 @@
   * `all-exec-hooks` container of functions to invoke if all states
   are `executed` and `:ctrl` is set back to `:ready` or `mon`
   * only porocess state if not already `:processed` which means: a
-  set-executed or set-error fn also should make `:processed` false" 
-  [v]
+  set-executed or set-error fn also should make `:processed` false
+  * state agent is now called `:proc`
+  * `:proc` contains all info: id, structname, task, (wow about repeat flag?)
+
+  TODO: tasks replace structs have to be guarded: `:@waittime` --> `:%waittime`" 
+  [id struct struct-name]
   (mapv (fn [{:keys [Ctrl Definition Title Description Element]} i]
           {:title Title  :elem Element :descr Description
            :proc (agent {:all-exec-hooks []
@@ -18,12 +22,13 @@
                          :state (flattenv
                                  (mapv (fn [s j] 
                                          (mapv (fn [t k] 
-                                                 {:ndx i :sdx j :pdx k
+                                                 {:id id :struct struct-name
+                                                  :ndx i :sdx j :pdx k
                                                   :is :ready
                                                   :task t})
                                                s (range)))
                                        Definition (range)))})})
-        v (range)))
+        struct (range)))
 
 (defn image
   "Note: The takedown fn is **not** `(shutdown-agents)`:
@@ -31,8 +36,8 @@
   executors and CIDER uses those global threadpools''"
   [{id :_id  {:keys [Exchange Container Definitions] :as mp} :Mp}]
   {(keyword id) (-> mp
-                    (assoc :Container (struct->struct-model Container))
-                    (assoc :Definitions (struct->struct-model Definitions))
+                    (assoc :Container (struct->struct-model id Container :cont))
+                    (assoc :Definitions (struct->struct-model id Definitions :defs))
                     (assoc :Documents (agent []))
                     (assoc :Exchange (agent Exchange))
                     (dissoc :Task))})
