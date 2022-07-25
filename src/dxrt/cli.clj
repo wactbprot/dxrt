df8dd1fd6aaef064fb6c383c8f3d897903dc42b5
Thomas Bock <thomas.bock@ptb.de>
2022-07-25 08:59:37 +0200

    merge

--- src/dxrt/cli.clj
+++ src/dxrt/cli.clj
@@ -1,10 +1,6 @@
 (ns dxrt.cli
   ^{:author "Thomas Bock <thomas.bock@ptb.de>"
     :doc "The dxrt cli."}
-<<<<<<< HEAD
-  (:require [libcdb.core :as db]
-            [integrant.core :as ig]))
-=======
   (:require [dxrt.core :as dx]
             [dxrt.system :as sys]
             [portal.api :as p]))
@@ -24,38 +20,14 @@
         f (fn [a] (assoc a :processed false))]
     (await a)
     (send a f))) 
->>>>>>> b979dc3962a8ca91bcd090b51c4e28feafea3e34
-
 
 (defn state [loc]
   (let [a (dx/proc-agent (model->) loc)]
     (send a dx/state loc)))
 
-<<<<<<< HEAD
-(defn  config [id image]
-
-  {:db/conn {:prot "http"
-             :host "localhost"
-             :port 5984
-             :name "vl_db"}
-
-   :db/mpd {:conn (ig/ref :db/conn)
-            :design "dbmp"
-            :view "mpdocs"}
-
-   :db/tasks {:conn (ig/ref :db/conn)
-              :design "dbmp"
-              :view "tasks"}
-
-   :image/container {:doc (ig/ref :mpd/doc) :image image}
-   :image/definition {:doc (ig/ref :mpd/doc) :image image}
-   :image/state {:doc (ig/ref :mpd/doc) :image image}
-   :image/sheduler {:image (ig/ref :image/state)}})
-=======
 (comment
   (proc (struct a-loc :mpd-ppc-gas_dosing :Container 0))
   (state (struct s-loc :mpd-ppc-gas_dosing :Container 0 0 0 :error)))
->>>>>>> b979dc3962a8ca91bcd090b51c4e28feafea3e34
 
 (comment
   (def p (p/open))
